851688a1e4e33ec18e249a8605dda961e34382d8
DavidPalaciosL <69604498+DavidPalaciosL@users.noreply.github.com>
2024-07-26 09:44:07 -0600

    AnnotationType

---
+++ src/PsiCollaboratorManager/Controllers/Annotation/AnnotationTypeController.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using PsiCollaborator.Data.Annotation.AnnotationType;
+using PsiCollaboratorManager.Models.Annotation;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace PsiCollaboratorManager.Controllers
+{
+    public class AnnotationTypeController : Controller
+    {
+        private IAnnotationTypeRepository _annotationTypeRepository;
+        private IMapper _mapper;
+        public AnnotationTypeController()
+        {
+            _annotationTypeRepository = new AnnotationTypeRepository();
+
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<AnnotationType, AnnotationTypeModel>();
+                cfg.CreateMap<AnnotationTypeModel, AnnotationType>();
+            });
+            _mapper = configuration.CreateMapper();
+        }
+        public ActionResult Index()
+        {
+            ViewBag.BasicTitle = "Tipos de Anotación";
+            return View();
+        }
+        public ActionResult GetAll()
+        {
+            List<AnnotationType> annotationTypes = _annotationTypeRepository.GetAll();
+            List<AnnotationTypeModel> annotationTypesModels = _mapper.Map<List<AnnotationTypeModel>>(annotationTypes);
+            return Json(new { rows = annotationTypesModels }, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Create()
+        {
+            ViewBag.BasicTitle = "Crear Tipo de Anotación";    
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Save(AnnotationTypeModel annotationTypeModel)
+        {
+            try
+            {
+                AnnotationType annotationType = _mapper.Map<AnnotationType>(annotationTypeModel);
+                _annotationTypeRepository.Save(annotationType);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public ActionResult Edit(int annotationTypeId)
+        {
+            ViewBag.BasicTitle = "Editar Tipo de Anotación";
+            AnnotationType annotationType = _annotationTypeRepository.GetById(annotationTypeId);
+            AnnotationTypeModel annotationTypeModel = _mapper.Map<AnnotationTypeModel>(annotationType);
+            return View(annotationTypeModel);
+        }
+    }
+}
---
+++ src/PsiCollaboratorManager/Models/Annotation/AnnotationTypeModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PsiCollaboratorManager.Models.Annotation
+{
+    public class AnnotationTypeModel
+    {
+        public int AnnotationTypeId { get; set; }
+        [Display(Name = "Nombre")]
+        public string TypeName { get; set; }
+        [Display(Name = "Valor en la Nota")]
+        public bool ValueInScore { get; set; }
+        [Display(Name = "Visible para el colaborador")]
+        public bool VisibleToCollaborator { get; set; }
+        [Display(Name = "Porcentaje")]
+        public double Percentage { get; set; }
+    }
+}
